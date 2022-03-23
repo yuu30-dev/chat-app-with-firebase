ee9e5aceb374c93997353f97463cad4882a8f56c
yuu30-dev <y.nagai0330@gmail.com>
2022-03-23 23:09:42 +0900

    add sign up page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,5 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import SignInAndRegistration from '../components/SignInAndRegistration'
-import styles from '../styles/Home.module.css'
+import SignIn from '../components/SignIn';
 
 export default function Home() {
-  return (
-   <SignInAndRegistration />
-  )
+  return <SignIn />;
 }
